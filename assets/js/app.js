26a88747317cb022e4ece5537044362b17e7d442
yemiwebby <yemiwebby@gmail.com>
2018-07-18 15:59:20 +0100

    Added bootstrap vue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
-
+import BootstrapVue from 'bootstrap-vue'
 import Example from './components/Example'
 
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
 /**
  * Create a fresh Vue Application instance
  */
--- assets/js/components/Example.vue
+++ assets/js/components/Example.vue
@@ -1,13 +1,29 @@
 <template>
     <div>
         <p>This is an example of a new components in VueJs</p>
+
+        <b-alert show dismissible> Hello {{ sample }} </b-alert>
+
+        <b-list-group>
+            <b-list-group-item href="http://apple.com">iPhone</b-list-group-item>
+            <b-list-group-item>OnePlus 3T</b-list-group-item>
+            <b-list-group-item>Samsung Galaxy 8</b-list-group-item>
+        </b-list-group>
     </div>
 </template>
 
 <script>
     export default {
 
-        name: "example"
+        name: "example",
+        data() {
+            return {
+               sample: ""
+            }
+        },
+        mounted: function() {
+            this.sample = "test test"
+        }
 
     }
 </script>
